ab0e36d9abd3d63d619bfadb70d0ccdefd318735
Paul Wilson <paulanthonywilson@gmail.com>
2013-07-30 11:27:02 +0100

    redirect to ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Correcthorsebatterystaple::Application.routes.draw do
-  root :to => "passwords#index"
-  match 'passwords', :to=>'passwords#create', :via=>'get'
+  require_ssl = !Rails.env.development?
+
+  scope :constraints => require_ssl ? { :protocol => "https" }  : {} do
+    root :to => "passwords#index"
+    match 'passwords', :to=>'passwords#create', :via=>'get'
+  end
+  match "*", :to => redirect { |_, request| "https://" + request.host_with_port + request.fullpath } if require_ssl
+  root :to=>redirect{|_, request| "https://#{request.host_with_port}/"} if require_ssl
 end
